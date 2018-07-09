dd09f0636044202ac3fa17cfdf84a19dd9e3b1ff
Matthew Kennedy <burnsidemk@gmail.com>
2018-07-09 12:08:50 -0500

    Disable toolbar mode

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(tool-bar-mode -1)
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives
@@ -140,8 +141,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (which-key projectile company-quickhelp slime-company flycheck-rust racer company cargo rust-mode ido-vertical-mode magit smex ido-completing-read+ flx-ido cider paredit use-package))))
+   '(which-key projectile company-quickhelp slime-company flycheck-rust racer company cargo rust-mode ido-vertical-mode magit smex ido-completing-read+ flx-ido cider paredit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
