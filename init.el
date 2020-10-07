e5b02fbd21cd2193841dae4ba5e2bda9864bd663
Matthew Kennedy <mattk@onprem.com>
2020-10-07 11:42:14 -0500

    selectrum config

--- init.el
+++ init.el
@@ -275,52 +275,62 @@
 ;;               (setq edit-server-new-frame nil)
 ;;               (edit-server-start)))
 
-(use-package ivy
-  :straight t
-  :config
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (use-package counsel :straight t)
-  (use-package counsel-projectile
-    :straight t
-    :config (counsel-projectile-mode))
-  (global-set-key (kbd "C-s") 'swiper-isearch)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "M-y") 'counsel-yank-pop)
-  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l") 'counsel-find-library)
-  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "<f2> j") 'counsel-set-variable)
-  (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-  (global-set-key (kbd "C-c v") 'ivy-push-view)
-  (global-set-key (kbd "C-c V") 'ivy-pop-view)
+;; (use-package ivy
+;;   :straight t
+;;   :config
+;;   (setq ivy-use-virtual-buffers t)
+;;   (setq ivy-count-format "(%d/%d) ")
+;;   (use-package counsel :straight t)
+;;   (use-package counsel-projectile
+;;     :straight t
+;;     :config (counsel-projectile-mode))
+;;   (global-set-key (kbd "C-s") 'swiper-isearch)
+;;   (global-set-key (kbd "M-x") 'counsel-M-x)
+;;   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;;   (global-set-key (kbd "M-y") 'counsel-yank-pop)
+;;   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;;   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;;   (global-set-key (kbd "<f1> l") 'counsel-find-library)
+;;   (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;;   (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;;   (global-set-key (kbd "<f2> j") 'counsel-set-variable)
+;;   (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+;;   (global-set-key (kbd "C-c v") 'ivy-push-view)
+;;   (global-set-key (kbd "C-c V") 'ivy-pop-view)
 
-  (global-set-key (kbd "C-c c") 'counsel-compile)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c L") 'counsel-git-log)
-  (global-set-key (kbd "C-c k") 'counsel-rg)
-  (global-set-key (kbd "C-c m") 'counsel-linux-app)
-  (global-set-key (kbd "C-c n") 'counsel-fzf)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-c J") 'counsel-file-jump)
-  ;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-  ;; (global-set-key (kbd "C-c w") 'counsel-wmctrl)
+;;   (global-set-key (kbd "C-c c") 'counsel-compile)
+;;   (global-set-key (kbd "C-c g") 'counsel-git)
+;;   (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;;   (global-set-key (kbd "C-c L") 'counsel-git-log)
+;;   (global-set-key (kbd "C-c k") 'counsel-rg)
+;;   (global-set-key (kbd "C-c m") 'counsel-linux-app)
+;;   (global-set-key (kbd "C-c n") 'counsel-fzf)
+;;   (global-set-key (kbd "C-x l") 'counsel-locate)
+;;   (global-set-key (kbd "C-c J") 'counsel-file-jump)
+;;   ;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;;   ;; (global-set-key (kbd "C-c w") 'counsel-wmctrl)
 
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "C-c b") 'counsel-bookmark)
-  (global-set-key (kbd "C-c d") 'counsel-descbinds)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c o") 'counsel-outline)
-  (global-set-key (kbd "C-c t") 'counsel-load-theme)
-  (global-set-key (kbd "C-c F") 'counsel-org-file))
+;;   (global-set-key (kbd "C-c C-r") 'ivy-resume)
+;;   (global-set-key (kbd "C-c b") 'counsel-bookmark)
+;;   (global-set-key (kbd "C-c d") 'counsel-descbinds)
+;;   (global-set-key (kbd "C-c g") 'counsel-git)
+;;   (global-set-key (kbd "C-c o") 'counsel-outline)
+;;   (global-set-key (kbd "C-c t") 'counsel-load-theme)
+;;   (global-set-key (kbd "C-c F") 'counsel-org-file))
 
 (use-package yaml-mode :straight t)
 (use-package hy-mode :straight t)
 (use-package elvish-mode :straight t)
+
+(use-package selectrum
+  :straight t
+  :config
+  (selectrum-mode 1)
+  (use-package selectrum-prescient
+    :straight t
+    :config 
+    (selectrum-prescient-mode +1)
+    (prescient-persist-mode +1)))
 
 (put 'downcase-region 'disabled nil)
 (put 'erase-buffer 'disabled nil)
