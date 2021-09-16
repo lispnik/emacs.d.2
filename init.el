8394a0d29bede322f356dc611f4e9b684a074394
Matthew Kennedy <mattk@onprem.com>
2021-09-16 13:01:46 -0500

    Fix font

--- init.el
+++ init.el
@@ -340,4 +340,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "JetBrains Mono" :foundry "nil" :slant normal :weight normal :height 130 :width normal)))))
