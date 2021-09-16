76e4f832aaea92275a08166e22df8c11b58c88f2
Matthew Kennedy <mattk@onprem.com>
2021-09-16 13:00:23 -0500

    fixups

--- init.el
+++ init.el
@@ -331,15 +331,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-<<<<<<< HEAD
  '(menu-bar-mode nil)
  '(show-paren-mode t)
- '(tool-bar-mode nil)
- '(yas-global-mode t))
-=======
- '(font-use-system-font t)
  '(tool-bar-mode nil))
->>>>>>> 39bab7294e469df6b30661fb6fec8cf36fdd0723
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
