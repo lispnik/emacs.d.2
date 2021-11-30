32a4e8f7ffb70ab7cc751d17068a6d83eafc2a5f
Matthew Kennedy <burnsidemk@gmail.com>
2021-11-30 15:12:46 -0600

    Safe variables

--- init.el
+++ init.el
@@ -299,6 +299,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((project-vc-merge-submodules)))
  '(show-paren-mode t))
 
 (custom-set-faces
