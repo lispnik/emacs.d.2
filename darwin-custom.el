6758e04986471d21ab850b1d38859e7f5576dfb8
Matthew Kennedy <burnsidemk@gmail.com>
2022-08-27 12:04:36 -0500

    darwin customizatiojn

--- darwin-custom.el
+++ darwin-custom.el
@@ -4,24 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
- '(safe-local-variable-values
-   '((Package . BORDEAUX-THREADS)
-     (Syntax . ANSI-Common-lisp)
-     (Syntax . ANSI-Common-Lisp)
-     (encoding . utf-8)
-     (Package . CLAWK-TEST)
-     (Base . 10)
-     (Package . CLAWK)
-     (Syntax . COMMON-LISP)
-     (sh-indent-comment . t)
-     (nasm-basic-offset . 2)
-     (nasm-basic-offset . 4)
-     (project-vc-merge-submodules)))
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(warning-suppress-log-types '((comp))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(default ((t (:family "Iosevka Nerd Font Mono" :foundry "nil" :slant normal :weight normal :height 160 :width normal)))))
+ '(default ((t (:family "Iosevka Nerd Font Mono" :foundry "nil" :slant normal :weight normal :height 160 :width normal)))))
